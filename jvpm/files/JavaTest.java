77612d15cc8c32014a0bbcfc40ca6b20ceb102fb
cwerner972 <46913629+cwerner972@users.noreply.github.com>
2019-04-07 10:06:51 -0600

    Updated Java Test file with scanner class and Hello World print

---
+++ jvpm/files/JavaTest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class JavaTest {
+
+   public static void main (String [] args) {
+   
+      Scanner Reader = new Scanner(System.in);
+      
+      int a;
+      int b;
+      int c;
+      double d;
+      byte e;
+      int myint;
+         
+      a = 1 + 2;
+      b = 3 / 4;
+      c = a + b;
+      c = a & b;
+      b = c * -1;
+      a = c - b;
+      c = -a;
+      c = a | b;
+      c = 16 % 5;
+      c = a << 1;
+      c = a >> 1;
+      c = a ^ b;
+      d = c;
+      
+      myint = Reader.nextInt();
+      System.out.println("This is my integer: " + myint);
+      System.out.println("Hello World");
+            
+   }
+}
